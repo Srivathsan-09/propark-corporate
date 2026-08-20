8dc19c8ab46eec855b6c971ef1d25014bdd2a5ec
Srivathsan-09 <srivathsan0906@gmail.com>
2026-08-20 23:44:51 +0530

    Make campus cards narrower: 3-column grid on xl screens

--- app/(admin)/admin/campuses/page.tsx
+++ app/(admin)/admin/campuses/page.tsx
@@ -752,7 +752,7 @@ export default function AdminCampusesPage() {
           </div>
 
           {/* Campus Cards List - Compact, High-Density & Overflow-Proof */}
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-3">
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-3">
             {filteredCampuses.map((campus) => (
               <div
                 key={campus._id || campus.campusId}
